4ceeb66f6bc655f2c64f87c999703d38a6d608ea
unknown <1219959303@qq.com>
2020-11-12 16:31:01 +0800

    李佳龙项目

---
+++ src/views/Zu2.vue
@@ -0,0 +1,232 @@
+<template>
+  <div id="app">
+    <Tou title="会话"></Tou>
+    <div class="cont">
+      <div class="div1">
+        <p class="p1">当前会话</p>
+        <p class="tab">
+          <el-button slot="reference">
+            <img
+              src="../assets/img/logo.png"
+              width="40px"
+              class="img"
+              align="left"
+            />
+            <p class="a">北京客户</p>
+            <p class="b">您好，我想咨询一下</p>
+          </el-button>
+        </p>
+        <p class="tab">
+          <el-button slot="reference">
+            <img
+              src="../assets/img/logo.png"
+              width="40px"
+              class="img"
+              align="left"
+            />
+            <p class="a">北京客户</p>
+            <p class="b">您好，我想咨询一下</p>
+          </el-button>
+        </p>
+        <p class="tab">
+          <el-button slot="reference">
+            <img
+              src="../assets/img/logo.png"
+              width="40px"
+              class="img"
+              align="left"
+            />
+            <p class="a">北京客户</p>
+            <p class="b">您好，我想咨询一下</p>
+          </el-button>
+        </p>
+        <p class="tab">
+          <el-button slot="reference">
+            <img
+              src="../assets/img/logo.png"
+              width="40px"
+              class="img"
+              align="left"
+            />
+            <p class="a">北京客户</p>
+            <p class="b">您好，我想咨询一下</p>
+          </el-button>
+        </p>
+        <p class="tab">
+          <el-button slot="reference">
+            <img
+              src="../assets/img/logo.png"
+              width="40px"
+              class="img"
+              align="left"
+            />
+            <p class="a">北京客户</p>
+            <p class="b">您好，我想咨询一下</p>
+          </el-button>
+        </p>
+        <p class="tab">
+          <el-button slot="reference">
+            <img
+              src="../assets/img/logo.png"
+              width="40px"
+              class="img"
+              align="left"
+            />
+            <p class="a">北京客户</p>
+            <p class="b">您好，我想咨询一下</p>
+          </el-button>
+        </p>
+        <p class="tab">
+          <el-button slot="reference">
+            <img
+              src="../assets/img/logo.png"
+              width="40px"
+              class="img"
+              align="left"
+            />
+            <p class="a">北京客户</p>
+            <p class="b">您好，我想咨询一下</p>
+          </el-button>
+        </p>
+        <p class="tab">
+          <el-button slot="reference">
+            <img
+              src="../assets/img/logo.png"
+              width="40px"
+              class="img"
+              align="left"
+            />
+            <p class="a">北京客户</p>
+            <p class="b">您好，我想咨询一下</p>
+          </el-button>
+        </p>
+      </div>
+
+      <div class="div2">
+        <div class="tou">
+          <div class="left1">
+            <span>北京市</span>
+            <span> 首次会话 </span>
+          </div>
+          <div class="right1">
+            <p>
+                <el-button type="text" @click="dialogFormVisible = true"
+              ><i class="el-icon-plus"></i>创建工单</el-button
+            >
+            <el-dialog title="新建工单" :visible.sync="dialogFormVisible">
+              <el-form :model="form">
+                <el-form-item label="活动名称" :label-width="formLabelWidth">
+                  <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="活动区域" :label-width="formLabelWidth">
+                  <el-select v-model="form.region" placeholder="请选择活动区域">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-form>
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false"
+                  >确 定</el-button
+                >
+              </div>
+            </el-dialog>
+            
+            </p>
+            
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Tou from "../components/Tou";
+export default {
+  name: "Zu2",
+  components: {
+    Tou,
+  },
+  data() {
+    return {
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+      formLabelWidth: "120px",
+    };
+  },
+};
+</script>
+<style scoped>
+.cont {
+  width: 1295px;
+  margin-left: 70px;
+  background: #e6f1ff;
+  margin-top: 60px;
+  float: right;
+}
+.div1 {
+  float: left;
+  background: #ffffff;
+  width: 300px;
+  margin-left: 30px;
+  margin-top: 10px;
+  border-radius: 5px;
+}
+.p1 {
+  padding: 20px;
+}
+.a {
+  width: 115px;
+  line-height: 25px;
+  padding-left: 0px;
+  float: left;
+}
+.tab .el-button {
+  width: 300px;
+  border: none;
+  height: 70px;
+}
+.b {
+  width: 165px;
+  font-size: 10px;
+  line-height: 25px;
+  float: left;
+}
+.div2 {
+  float: left;
+  width: 955px;
+  margin-top: 10px;
+  margin-left: 10px;
+  background: #ffffff;
+  height: 600px;
+}
+.tou {
+  width: 100%;
+  height: 50px;
+  background: red;
+  line-height: 50px;
+}
+.left1 {
+  float: left;
+  padding-left: 10px;
+}
+.left1 span:nth-of-type(2) {
+  font-size: 10px;
+  color: #cccccc;
+}
+.right1{
+    float: right;
+}
+</style>
---
+++ src/views/Zu3.vue
@@ -0,0 +1,301 @@
+<template>
+  <div id="app">
+    <Tou title="访客"></Tou>
+    <div class="cont">
+      <div class="div1">
+
+        <div class="sd">
+          <div>
+            <el-select v-model="value" placeholder="全部客服">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </div>
+
+          <div>
+            <el-select v-model="value" placeholder="全部来源">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </div>
+          <div>
+            <el-select v-model="value" placeholder="全部终端">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </div>
+        </div>
+
+        <div class="block">
+          <el-date-picker
+            v-model="value"
+            type="daterange"
+            start-placeholder="请选择日期"
+            end-placeholder="请选择日期"
+            :default-time="['00:00:00', '23:59:59']"
+          >
+          </el-date-picker>
+        </div>
+
+        <div class="ss">
+          <input
+            type="text"
+            class="input"
+            placeholder="请输入关键词搜索"
+          /><button class="btn">搜索</button>
+        </div>
+
+        <div class="dao">导出数据</div>
+
+        <div class="div2">
+          <el-table :data="tableData" style="width: 100%;">
+            <el-table-column prop="date" label="访客名称" align="center" >
+            </el-table-column>
+            <el-table-column prop="name" label="来源" align="center">
+            </el-table-column>
+            <el-table-column prop="address" label="终端" align="center">
+            </el-table-column>
+            <el-table-column prop="address2" label="访问时间" align="center">
+            </el-table-column>
+            <el-table-column prop="address3" label="访问时长" align="center">
+            </el-table-column>
+            <el-table-column prop="address4" label="IP地址" align="center">
+            </el-table-column>
+            <el-table-column prop="address5" label="访客状态" align="center">
+            </el-table-column>
+            <el-table-column prop="address6" label="操作" align="center">
+            </el-table-column>
+          </el-table>
+        </div>
+        <div class="div3">
+          <el-pagination background layout="prev, pager, next" :total="1000">
+          </el-pagination>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Tou from "../components/Tou";
+export default {
+  name: "Zu3",
+  components: {
+    Tou,
+  },
+  data() {
+    return {
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+      ],
+      tableData: [
+        {
+          date: "河北省秦皇岛市区",
+          name: "电脑",
+          address: "微信",
+          address2: "2020/03/09 10:55",
+          address3: "8分45秒",
+          address4: "132.131.22.1",
+          address5: "在线",
+          address6: "发起会话",
+        },
+        {
+          date: "河北省秦皇岛市区",
+          name: "电脑",
+          address: "微信",
+          address2: "2020/03/09 10:55",
+          address3: "8分45秒",
+          address4: "132.131.22.1",
+          address5: "在线",
+          address6: "发起会话",
+        },
+        {
+          date: "河北省秦皇岛市区",
+          name: "电脑",
+          address: "微信",
+          address2: "2020/03/09 10:55",
+          address3: "8分45秒",
+          address4: "132.131.22.1",
+          address5: "在线",
+          address6: "发起会话",
+        },
+        {
+          date: "河北省秦皇岛市区",
+          name: "电脑",
+          address: "微信",
+          address2: "2020/03/09 10:55",
+          address3: "8分45秒",
+          address4: "132.131.22.1",
+          address5: "在线",
+          address6: "发起会话",
+        },
+        {
+          date: "河北省秦皇岛市区",
+          name: "电脑",
+          address: "微信",
+          address2: "2020/03/09 10:55",
+          address3: "8分45秒",
+          address4: "132.131.22.1",
+          address5: "在线",
+          address6: "发起会话",
+        },
+        {
+          date: "河北省秦皇岛市区",
+          name: "电脑",
+          address: "微信",
+          address2: "2020/03/09 10:55",
+          address3: "8分45秒",
+          address4: "132.131.22.1",
+          address5: "在线",
+          address6: "发起会话",
+        },
+        {
+          date: "河北省秦皇岛市区",
+          name: "电脑",
+          address: "微信",
+          address2: "2020/03/09 10:55",
+          address3: "8分45秒",
+          address4: "132.131.22.1",
+          address5: "在线",
+          address6: "发起会话",
+        },
+        {
+          date: "河北省秦皇岛市区",
+          name: "电脑",
+          address: "微信",
+          address2: "2020/03/09 10:55",
+          address3: "8分45秒",
+          address4: "132.131.22.1",
+          address5: "在线",
+          address6: "发起会话",
+        },
+        {
+          date: "河北省秦皇岛市区",
+          name: "电脑",
+          address: "微信",
+          address2: "2020/03/09 10:55",
+          address3: "8分45秒",
+          address4: "132.131.22.1",
+          address5: "在线",
+          address6: "发起会话",
+        },
+      ],
+      value: "",
+    };
+  },
+};
+</script>
+<style  scoped>
+.cont{
+  width: 1295px;
+  margin-left: 70px;
+  background: #e6f1ff;
+  margin-top: 50px;
+  float: right;
+}
+.div1 {
+  width: 1280px;
+  background: #ffffff;
+  margin-top: 20px;
+    float: right;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409eff;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+.demonstration {
+  display: block;
+  color: #8492a6;
+  font-size: 14px;
+  margin-bottom: 20px;
+}
+.sd {
+  width: 490px;
+  float: left;
+}
+.sd div {
+  width: 150px;
+  margin: 5px 5px;
+  float: left;
+}
+.block {
+  float: left;
+  margin: 10px 0px;
+  background: #f2f2f2;
+}
+.ss {
+  float: left;
+  margin: 10px 10px;
+}
+.input {
+  background: #f2f2f2;
+  border: none;
+  width: 230px;
+  height: 40px;
+}
+.btn {
+  width: 60px;
+  height: 40px;
+  background: #006eff;
+  border: none;
+  color: #ffffff;
+  cursor: pointer;
+}
+.dao {
+  text-align: center;
+  width: 100px;
+  margin: 10px 10px;
+  border: 1px solid #999999;
+  float: left;
+  height: 40px;
+  line-height: 40px;
+  color: #999999;
+}
+.dao:hover {
+  border: 1px solid #006eff;
+  color: #006eff;
+  cursor: pointer;
+}
+.div2{
+  clear: both;
+  height: 500px;
+}
+.div3{
+  float: right;
+  clear: both;
+  margin-right: 50px;
+  margin-top: 20px;
+}
+</style>
